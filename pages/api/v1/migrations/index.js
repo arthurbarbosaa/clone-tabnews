9103e37de5b1176284ef938a35d952f1171dbf74
arthur mac <arthur.com50@gmail.com>
2024-12-10 21:33:29 -0300

    adds message in response from `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -36,7 +36,7 @@ export default async function migrations(request, response) {
       return response.status(200).json(migratedMigrations);
     }
 
-    return response.status(405).end();
+    return response.status(405).json("Method not alowed.");
   } catch (error) {
     return response.status(500).json(error);
   } finally {
