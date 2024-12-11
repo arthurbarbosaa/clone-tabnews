b6021c43e88d2b5db4bc37ea6216028aa5e77bf8
arthur mac <arthur.com50@gmail.com>
2024-12-10 21:51:44 -0300

    refactor error handling in migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,13 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not alowed`,
+    });
+  }
+
   let dbClient;
 
   try {
@@ -35,10 +42,9 @@ export default async function migrations(request, response) {
 
       return response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).json("Method not alowed.");
   } catch (error) {
-    return response.status(500).json(error);
+    console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }
