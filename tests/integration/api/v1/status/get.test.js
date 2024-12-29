be4fdf57ff9adb926089f5f1b615801ae6f0bbf0
arthur mac <arthur.com50@gmail.com>
2024-12-29 17:22:17 -0300

    test: rename all descriotions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitFroAllServices();
 });
 
-test("GET to /api/v1/status shold return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status/");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retriving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status/");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.postgres_version).toBe("16.0");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.using_connections).toBe(1);
+      expect(responseBody.dependencies.database.postgres_version).toBe("16.0");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.using_connections).toBe(1);
+    });
+  });
 });
